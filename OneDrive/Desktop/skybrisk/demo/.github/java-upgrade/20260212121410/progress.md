640e8e75844c73aaf3d04530f305526afac6524b
MVSK <venkatasaikumarmasanam@gmail.com>
2026-06-06 18:14:05 +0530

    Delete OneDrive/Desktop directory

--- OneDrive/Desktop/skybrisk/demo/.github/java-upgrade/20260212121410/progress.md
+++
@@ -1,7 +0,0 @@
-# Upgrade Progress
-
-  ### ⏳ Generate Upgrade Plan ...Running
-  - [[View Log]](logs\1.generatePlan.log)
-  
-  - ###
-    ### ✅ Install JDK 17
--- OneDrive/Desktop/smartshop/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer">
-      <p>© 2026 SmartShop</p>
-    </footer>
-  );
-}
-
-export default Footer;
